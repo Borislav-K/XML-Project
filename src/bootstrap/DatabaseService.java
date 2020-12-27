265c580e0eedcf327f7ac1ce6dd811e20e4c23c3
Bobi <bobissss84@gmail.com>
2020-12-27 12:43:18 +0200

    Change DB name and filesystem folder to database

--- src/bootstrap/DatabaseService.java
+++ src/bootstrap/DatabaseService.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseService {
 
-    private static final String JDBC_URL = "jdbc:h2:./resources/test";
+    private static final String JDBC_URL = "jdbc:h2:./database/market";
     private static final String DATABASE_USERNAME = "sa";
     private static final String DATABASE_PASSWORD = "";
 
